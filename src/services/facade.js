8a7e82dcb9a320b660894e95d1252191dad141ad
Emily Nieves <nemily1710@gmail.com>
2024-11-10 18:45:58 -0500

    feat: :sparkles: Se deja plantilla de facade

---
+++ src/services/facade.js
@@ -0,0 +1,42 @@
+import apiService from '../shared/fetch/apiService';
+
+class LibraryFacade {
+    async buscarLibros(filtros) {
+        // Construye la consulta de búsqueda usando SearchBuilder
+        const query = new SearchBuilder().setAuthor(filtros.author).setGenre(filtros.genre).setAvailability(filtros.isAvailable).build();
+
+        return await apiService.fetch('/books', {
+            method: 'POST',
+            body: JSON.stringify(query),
+        });
+    }
+
+    async obtenerLibroPorId(id) {
+        return await apiService.fetch(`/books/${id}`);
+    }
+
+    async actualizarLibro(id, data) {
+        return await apiService.fetch(`/books/${id}`, {
+            method: 'PUT',
+            body: JSON.stringify(data),
+        });
+    }
+
+    async agregarLibro(data) {
+        return await apiService.fetch('/books', {
+            method: 'POST',
+            body: JSON.stringify(data),
+        });
+    }
+
+    async eliminarLibro(id) {
+        return await apiService.fetch(`/books/${id}`, {
+            method: 'DELETE',
+        });
+    }
+}
+
+// Exporta una instancia única (Singleton) de LibraryFacade para su uso en toda la aplicación
+const libraryFacade = new LibraryFacade();
+Object.freeze(libraryFacade);
+export default libraryFacade;
