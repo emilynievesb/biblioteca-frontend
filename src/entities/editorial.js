72e19f9a6dfbd36d7ab96576a54efb8ce8d1c253
ADEP-123 <andep210399@gmail.com>
2024-11-10 20:05:46 -0500

    refactor: :recycle: deleting unnecessary methods

--- src/entities/editorial.js
+++ src/entities/editorial.js
@@ -2,45 +2,7 @@ class Editorial {
     nombre;
     nacionalidad;
 
-    constructor() {
-        this.queryCreate = `
-            mutation {
-                createEditorial(data: { nombre: "{0}", nacionalidad: "{1}" }) {
-                    data {
-                        id
-                        attributes {
-                            nombre
-                            nacionalidad
-                        }
-                    }
-                }
-            }
-        `;
-        
-        this.queryUpdate = `
-            mutation {
-                updateEditorial(id: {0}, data: { nombre: "{1}", nacionalidad: "{2}" }) {
-                    data {
-                        id
-                        attributes {
-                            nombre
-                            nacionalidad
-                        }
-                    }
-                }
-            }
-        `;
-        
-        this.queryDelete = `
-            mutation {
-                deleteEditorial(id: {0}) {
-                    data {
-                        id
-                    }
-                }
-            }
-        `;
-        
+    constructor() {      
         this.queryFetchAll = `
             query {
                 editorials {
