44c19f5064e807ab130ce6b32a9261cd85453480
Emily Nieves <nemily1710@gmail.com>
2024-11-11 12:29:25 -0500

    feat: :sparkles: Se crea controlador para usuarios

---
+++ src/app/controllers/users.controller.js
@@ -0,0 +1,37 @@
+import User from '../../entities/user';
+import UsersFacade from '../../services/user.facade';
+const singup = async (data) => {
+    const { name, surname, address, phone, email, day, month, year, username, password } = data;
+    console.log(new Date(`${day}/${month}/${year}`).toISOString().split('T')[0]);
+
+    const user = new User();
+    const userFacade = new UsersFacade();
+    user.setNombres(name);
+    user.setApellidos(surname);
+    user.setDireccion(address);
+    user.setTelefono(phone);
+    user.setCorreoElectronico(email);
+    user.setFechaNacimiento(new Date(`${day}/${month}/${year}`).toISOString().split('T')[0]);
+    user.setNomUsuario(username);
+    user.setContrasena(password);
+    user.setRol('usuario');
+    let queryValidate = user.buildQueryValidate();
+    console.log(queryValidate);
+    let responseValidate = await userFacade.fetchPost(queryValidate);
+    if (responseValidate.data.usersPermissionsUsers.data.length === 0) {
+        let queryUser = user.buildQueryRegister();
+        console.log(queryUser);
+        let responseUser = await userFacade.fetchPost(queryUser);
+        user.setId(responseUser.data.register.user.id);
+        let queryUserInfo = user.buildQueryRegisterUser();
+        console.log(queryUserInfo);
+        let responseUsuario = await userFacade.fetchPost(queryUserInfo);
+        console.log(responseValidate);
+        console.log(responseUser);
+        console.log(responseUsuario);
+        return 'okkkkkkkkkkkkkkkkkkkkkkkkkk';
+    } else {
+        return 'Ya existe el nombre de usuario o el email en la base de datos';
+    }
+};
+export default singup;
