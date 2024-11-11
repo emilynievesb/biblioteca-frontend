d89523f661543cf2b2c6f3d83865eb1abf8618ab
Emily Nieves <nemily1710@gmail.com>
2024-11-11 17:49:30 -0500

    feat: :sparkles: Se realizan consultas y controladores para registrar y buscar libros

---
+++ src/app/controllers/book.controller.js
@@ -0,0 +1,25 @@
+import Book from '@/entities/book';
+import BooksFacade from '@/services/books.facade';
+
+const registerBook = async (data) => {
+    const { isbn, titulo, editorial, idioma, edicion, categoria, resumen, autors } = data;
+    const book = new Book();
+    book.setIsbn(isbn);
+    book.setTitulo(titulo);
+    book.setEditorial(editorial);
+    book.setIdioma(idioma);
+    book.setEdicion(edicion);
+    book.setCategoria(categoria);
+    book.setResumen(resumen);
+    book.setAutors(autors);
+    const queryBooks = book.buildQueryRegister();
+    const bookFacade = new BooksFacade();
+    return await bookFacade.fetchPost(queryBooks);
+};
+const searchBooks = async () => {
+    const book = new Book();
+    const queryBooks = book.buildQueryFetchAll();
+    const bookFacade = new BooksFacade();
+    return await bookFacade.fetchPost(queryBooks);
+};
+export { searchBooks, registerBook };
--- src/services/books.facade.js
+++ src/services/books.facade.js
@@ -1,42 +1,13 @@
 import apiService from '../shared/fetch/apiService';
 
 class BooksFacade {
-    async buscarLibros(filtros) {
-        // Construye la consulta de búsqueda usando SearchBuilder
-        const query = new SearchBuilder().setAuthor(filtros.author).setGenre(filtros.genre).setAvailability(filtros.isAvailable).build();
-
-        return await apiService.fetch('/books', {
+    async fetchPost(query) {
+        return await apiService.fetch(`/graphql`, {
             method: 'POST',
-            body: JSON.stringify(query),
-        });
-    }
-
-    async obtenerLibroPorId(id) {
-        return await apiService.fetch(`/books/${id}`);
-    }
-
-    async actualizarLibro(id, data) {
-        return await apiService.fetch(`/books/${id}`, {
-            method: 'PUT',
-            body: JSON.stringify(data),
-        });
-    }
-
-    async agregarLibro(data) {
-        return await apiService.fetch('/books', {
-            method: 'POST',
-            body: JSON.stringify(data),
-        });
-    }
-
-    async eliminarLibro(id) {
-        return await apiService.fetch(`/books/${id}`, {
-            method: 'DELETE',
+            // cache: 'no-store',
+            body: JSON.stringify({ query }),
         });
     }
 }
 
-// Exporta una instancia única (Singleton) de LibraryFacade para su uso en toda la aplicación
-const booksFacade = new BooksFacade();
-Object.freeze(booksFacade);
-export default booksFacade;
+export default BooksFacade;
